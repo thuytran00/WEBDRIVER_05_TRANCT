560b2db90bc03cf5ce6fc4b4fe1a6a1acf713117
tranct <o0thuytran0o@gmail.>
2018-07-18 14:12:48 +0700

    Demo ngay 1

---
+++ src/selenium_api/Topic_01_CheckEnvironment.java
@@ -0,0 +1,38 @@
+package selenium_api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class Topic_01_CheckEnvironment {
+  WebDriver driver;
+  
+	@Test
+  public void TestCase() {
+	  String homePageTitle = driver.getTitle();
+	  Assert.assertEquals(homePageTitle, "Guru99 Bank Home Page");
+	  
+	  String homePageUrl = driver.getCurrentUrl();
+	  Assert.assertEquals(homePageUrl, "http://demo.guru99.com/v4/");
+	  
+	  
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  driver = new FirefoxDriver();
+	  driver.get("http://demo.guru99.com/v4/");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.manage().window().maximize();
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
