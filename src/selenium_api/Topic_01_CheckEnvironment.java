8600dfbbc4d3fba92df0ea9accd567889b1ff65c
Chung Thuy Tran <o0thuytran0o@gmail.com>
2018-07-18 14:37:46 +0700

    Check itergrate github vs slack

--- src/selenium_api/Topic_01_CheckEnvironment.java
+++ src/selenium_api/Topic_01_CheckEnvironment.java
@@ -14,9 +14,10 @@ public class Topic_01_CheckEnvironment {
   
 	@Test
   public void TestCase() {
+	 System.out.println("Check homepage title");
 	  String homePageTitle = driver.getTitle();
 	  Assert.assertEquals(homePageTitle, "Guru99 Bank Home Page");
-	  
+	  System.out.println("Check homepage url");
 	  String homePageUrl = driver.getCurrentUrl();
 	  Assert.assertEquals(homePageUrl, "http://demo.guru99.com/v4/");
 	  
